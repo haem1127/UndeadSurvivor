15478a452d0f04abccfba7e86d1222cff617470d
haem1127 <pms010506@gmail.com>
2025-04-02 14:28:18 +0900

    초기 커밋

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,16 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    public float gameTime;
+    public float maxGameTime = 2 * 10f;
     public PoolManager pool;
     public Player player;
 
     void Awake()
     {
         instance = this;
+    }
+
+    private void Update()
+    {
+        gameTime += Time.deltaTime;
+
+        if (gameTime > maxGameTime)
+        {
+            gameTime = maxGameTime;
+        }
     }
 
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,6 +6,7 @@ public class Spawner : MonoBehaviour
 {
     public Transform[] spawnPoint;
 
+    int level;
     // 소환 타이머를 위한 변수 선언
     float timer;
 
@@ -20,9 +21,12 @@ public class Spawner : MonoBehaviour
         // += 우변을 좌변에 계속 더하다
         // 타이머 변수에는 deltaTime을 계속 더하기
         timer += Time.deltaTime;
+        // FloorToInt는 소수점 아래는 버리고 Int형으로 바꾸는 함수이다
+        // CeilToInt는 소수점 아래를 올리고 Int형으로 바꾸는 함수이다
+        level = Mathf.FloorToInt(GameManager.instance.gameTime / 10f);
 
         // 타이머가 일정 시간 값에 도달하면 소환하도록 작성
-        if(timer > 0.2f)
+        if(timer > (level == 0 ? 0.5f : 0.2f))
         {
             timer= 0;
             Spawn();
@@ -30,8 +34,8 @@ public class Spawner : MonoBehaviour
 
         void Spawn()
         {
-            // 풀 함수 안에는 랜덤 인자를 넣도록 변경
-            GameObject enemy = GameManager.instance.pool.Get(Random.Range(0,2));
+            // 풀 함수 안에는 랜덤 인자를 넣도록 변경 > 풀링에서 가져오는 함수에도 레벨 적용
+            GameObject enemy = GameManager.instance.pool.Get(level);
             // 자신을 제외한 자식 오브젝트에서부터 선택되도록 랜덤 시작은 1부터
             enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
         }
